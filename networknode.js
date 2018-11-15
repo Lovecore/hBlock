c27179715bf9058249b522e0108833151017c465
Lovecore <ncanino@gmail.com>
2018-11-15 15:33:43 -0500

    Add files via upload

---
+++ networknode.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+const nodeAddress = uuid().split('-').join('');  //create a node ID
+const port = process.argv[2]; //reference our start command, last position of the argument
+
+//create a chain
+const pkizzle = new Blockchain(); 
+
+//needed for req.box
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+//create api endpoints 
+app.get('/blockchain', function(req, res) {
+    res.send(pkizzle);
+})
+
+app.post('/transaction', function(req, res) {
+    const blockIndex = pkizzle.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.`});
+})
+
+app.get('/mine', function(req,res) {
+    const lastBlock = pkizzle.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: pkizzle.pendingTransactions,
+        index: lastBlock['index'] + 1,
+        timestamp: Date.now()
+    };
+
+    const nonce = pkizzle.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = pkizzle.hashBlock(previousBlockHash, currentBlockData, nonce);
+    
+    //create transaction reward
+    pkizzle.createNewTransaction(12.5, "00", nodeAddress); 
+
+    //create the block and respond
+    const newBlock = pkizzle.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
+});
+
+//create a random gen port for testing
+app.listen(port, function() {
+    console.log(`Listening on port ${port}...`); //let me know the port is ready cause im dumb
+})
