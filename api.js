006c857c236a116de9823cf40b49b0e53d1254b4
Lovecore <ncanino@gmail.com>
2018-11-15 15:13:25 -0500

    Add files via upload

--- api.js
+++ api.js
@@ -1,18 +1,49 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const uuid = require('uuid/v1');
+const nodeAddress = uuid().split('-').join('');  //create a node ID
 
-//create api endpoints
-app.get('blockchain', function(req, res) {
+//create a chain
+const pkizzle = new Blockchain(); 
 
+//needed for req.box
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+//create api endpoints 
+app.get('/blockchain', function(req, res) {
+    res.send(pkizzle);
 })
 
-app.get('/transaction', function(req, res ) {
-
+app.post('/transaction', function(req, res) {
+    const blockIndex = pkizzle.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.`});
 })
 
 app.get('/mine', function(req,res) {
+    const lastBlock = pkizzle.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: pkizzle.pendingTransactions,
+        index: lastBlock['index'] + 1,
+        timestamp: Date.now()
+    };
 
-})
+    const nonce = pkizzle.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = pkizzle.hashBlock(previousBlockHash, currentBlockData, nonce);
+    
+    //create transaction reward
+    pkizzle.createNewTransaction(12.5, "00", nodeAddress); 
+
+    //create the block and respond
+    const newBlock = pkizzle.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
+});
 
  
 app.listen(3000, function() {
--- test.js
+++ test.js
@@ -1,5 +1,43 @@
 const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
 
-const pkizzle = new Blockchain();
+const previousBlockHash = ' kfjlksdjfa834hn2nd';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'MICHELLE342432423',
+        recipient: 'NICK8937429834hfh2'
+    },
+    {
+        amount: 30,
+        sender: 'TOM83hnf892n9d2edw',
+        recipient: 'MIKEfsd324fsdfsdf323'
+    },
+    {
+        amount: 66,
+        sender: 'ROYndjsncjkdnfksjdyf7893',
+        recipient: 'sdfsdsdfse232d2d'
+    }
+];
 
-console.log(pkizzle);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 150884));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bitcoin.hashBlock();
+
+
+
+console.log(bitcoin);
